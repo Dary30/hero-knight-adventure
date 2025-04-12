59df3e0ab35ea532c2c0d7cee17e98a85fcdcb49
Darius O. <72305773+Dary30@users.noreply.github.com>
2025-04-12 14:23:23 +0300

    Fix portal logic
    Added a delay before switching the scene.

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -3,5 +3,10 @@ extends Node2D
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
-		#get_tree().change_scene_to_file("res://scenes/levels/Level" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
-		print("res://scenes/levels/Level" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
+		var current_level = int(get_tree().current_scene.name)
+		var next_level_path = "res://scenes/levels/Level" + str(current_level + 1) + ".tscn"
+		call_deferred("change_level", next_level_path)
+
+
+func change_level(path: String) -> void:
+	get_tree().change_scene_to_file(path)
