c585fdbe4a5cd5ca225449c01b54a00bf5273a2d
Darius O. <72305773+Dary30@users.noreply.github.com>
2025-05-08 01:10:52 +0300

    Improve player handling
    Added a few things to improve the player handling:
    -jump buffering
    -coyote time
    -fall multiplier to fall 2 times faster

--- src/characters/player.gd
+++ src/characters/player.gd
@@ -4,10 +4,15 @@ class_name Player
 const SPEED = 130.0
 
 @export var jump_force = -300
-@export_range(0, 1) var deceleration = 0.2
+@export_range(0, 1) var deceleration = 0.5
 @export_range(0, 1) var decelerate_on_jump_release = 0.5
+@export var fall_multiplier = 2.0
+@export var coyote_time = 0.06
+@export var jump_buffer_time = 0.1
 
 var can_move: bool = true
+var coyote_timer = 0.0
+var jump_buffer_timer = 0.0
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -35,13 +40,29 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 	
-	# Add the gravity.
+	# Timers.
+	if is_on_floor():
+		coyote_timer = coyote_time
+	else:
+		coyote_timer -= delta
+	
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = jump_buffer_time
+	else:
+		jump_buffer_timer -= delta
+	
+	# Add gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y > 0:
+			velocity += get_gravity() * fall_multiplier * delta
+		else:
+			velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if jump_buffer_timer > 0 and coyote_timer > 0:
 		velocity.y = jump_force
+		jump_buffer_timer = 0
+		coyote_timer = 0
 
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
