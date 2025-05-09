6db0e0ad3d1eb1777bb9b15759739b484e1eec24
Darius O. <72305773+Dary30@users.noreply.github.com>
2025-05-09 14:26:13 +0300

    Add finishing touches
    Changed the game icon.
    Enabled music autoplay.
    The game now starts in maximized window instead of windowed.

--- src/ui/fullscreen_control.gd
+++ src/ui/fullscreen_control.gd
@@ -5,4 +5,4 @@ func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
